1afbb2b6d75b858699206d4433e0b104f72dcb89
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-10-27 13:57:21 -0500

    feat: ✨ buffer bar (barbar) removed and improve tabsupport

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -8,40 +8,6 @@ return {
 		lazy = true,
 	},
 	{
-		"folke/persistence.nvim",
-		event = "BufReadPre",
-		opts = { options = vim.opt.sessionoptions:get() },
-		keys = {
-			{
-				"<leader>qs",
-				function()
-					require("persistence").load()
-				end,
-				desc = "Restore Session",
-			},
-			{
-				"<leader>ql",
-				function()
-					require("persistence").load({ last = true })
-				end,
-				desc = "Restore Last Session",
-			},
-			{
-				"<leader>qd",
-				function()
-					require("persistence").stop()
-				end,
-				desc = "Don't Save Current Session",
-			},
-		},
-		config = function()
-			require("persistence").setup({
-				dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"),
-				options = { "buffers", "curdir", "winsize" },
-			})
-		end,
-	},
-	{
 		-- Icons for file types
 		"nvim-tree/nvim-web-devicons",
 		lazy = true,
