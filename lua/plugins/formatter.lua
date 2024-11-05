3b43e69c98b3ee117872683a64ed0c9e2ce20efc
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-11-05 18:40:29 -0500

    feat: ✨ Remove auto formatter and added 4 indent for java

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,18 +3,22 @@ return {
 		"stevearc/conform.nvim",
 		opts = {},
 		config = function()
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			})
+			vim.api.nvim_create_user_command("Format", function(args)
+				local range = nil
+				if args.count ~= -1 then
+					local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+					range = {
+						start = { args.line1, 0 },
+						["end"] = { args.line2, end_line:len() },
+					}
+				end
+				require("conform").format({ async = true, lsp_format = "fallback", range = range })
+			end, { range = true })
+
+			-- Format
+			vim.keymap.set("n", "<leader>fc", ":Format<CR>", { desc = "Split Window Right", silent = true })
 
 			require("conform").setup({
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_format = "fallback",
-				},
 				formatters_by_ft = {
 					lua = { "stylua" },
 					-- Conform will run multiple formatters sequentially
@@ -38,7 +42,8 @@ return {
 					angular = { "prettierd", "prettier", stop_after_first = true },
 
 					-- Java
-					java = { "google-java-format" },
+					java = { "jdtls" },
+					prepend_args = { "--use-tabs" },
 				},
 			})
 		end,
--- lua/settings.lua
+++ lua/settings.lua
@@ -25,3 +25,5 @@ opt.undolevels = 10000
 opt.updatetime = 50
 
 vim.opt.wrap = false
+
+vim.cmd("autocmd FileType java setlocal et ts=4 sw=4 sts=4 <CR>")
