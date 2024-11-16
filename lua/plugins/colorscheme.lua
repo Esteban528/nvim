ed7165f6999ca0b0a97af24474ad8c3b926c1a46
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-11-15 21:37:54 -0500

    feat(update): 🐛 update and change color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  "EdenEast/nightfox.nvim",
+  'kvrohit/rasmus.nvim',
 
 	priority = 1000,
 	config = function()
 		vim.o.background = "dark"
     -- vim.opt.termguicolors = true
-		vim.cmd("colorscheme carbonfox")
+		vim.cmd("colorscheme rasmus")
 	end,
 }
