65ad14ec6d2d5437c49bd1cab154c3fc8bdb3487
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-03-21 17:46:29 -0500

    chore: update and file name updates

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -40,7 +40,7 @@ return {
 			require("mini.statusline").setup()
 			require("mini.pairs").setup()
 			require("mini.snippets").setup({})
-			-- vim.keymap.set("n", "<leader>n", ":lua MiniFiles.open() <CR>")
+			vim.keymap.set("n", "<leader>n", ":lua MiniFiles.open() <CR>")
 
 			require("mini.icons").setup()
 			MiniIcons.mock_nvim_web_devicons()
