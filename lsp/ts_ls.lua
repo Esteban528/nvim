86daeb9b6b72c3bed0206e4e50275d57cdd0c3a0
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-07-10 21:06:26 -0500

    chore: ts_ls updated

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -25,10 +25,36 @@ return {
       return vim.NIL
     end,
   },
-  on_attach = function(client)
+  commands = {
+    ['editor.action.showReferences'] = function(command, ctx)
+      local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+      local file_uri, position, references = unpack(command.arguments)
+
+      local quickfix_items = vim.lsp.util.locations_to_items(references, client.offset_encoding)
+      vim.fn.setqflist({}, ' ', {
+        title = command.title,
+        items = quickfix_items,
+        context = {
+          command = command,
+          bufnr = ctx.bufnr,
+        },
+      })
+
+      vim.lsp.util.show_document({
+        uri = file_uri,
+        range = {
+          start = position,
+          ['end'] = position,
+        },
+      }, client.offset_encoding)
+
+      vim.cmd('botright copen')
+    end,
+  },
+  on_attach = function(client, bufnr)
     -- ts_ls provides `source.*` code actions that apply to the whole file. These only appear in
     -- `vim.lsp.buf.code_action()` if specified in `context.only`.
-    vim.api.nvim_buf_create_user_command(0, 'LspTypescriptSourceAction', function()
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspTypescriptSourceAction', function()
       local source_actions = vim.tbl_filter(function(action)
         return vim.startswith(action, 'source.')
       end, client.server_capabilities.codeActionProvider.codeActionKinds)
