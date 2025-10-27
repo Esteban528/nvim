a16f586b336e69d3f81709a269637f16258ec53e
Esteban Gonzalez <esteban@estebandev.xyz>
2025-10-27 12:24:19 -0500

    chore(colorscheme): changed to rosepine

--- lua/palette.lua
+++ lua/palette.lua
@@ -1,23 +1,24 @@
 local M = {}
 
 M.palette = { -- My Color palette
-  base00 = "#100f0f", -- bg
-  base01 = "#1c1b1a", -- bg-2
-  base02 = "#282726", -- ui
-  base03 = "#343331", -- ui-2
-  base04 = "#403e3c", -- ui-3
-  base06 = "#878580", -- tx-2
-  base05 = "#cecdc3", -- tx
-  base07 = "#575653", -- tx-3
+	base00 = "#191724",
+	base01 = "#1f1d2e",
+	base02 = "#26233a",
+	base03 = "#6e6a86",
+	base04 = "#908caa",
+	base05 = "#e0def4",
+	base06 = "#e0def4",
+	base07 = "#524f67",
+	base08 = "#eb6f92",
+	base09 = "#f6c177",
+	base0A = "#ebbcba",
+	base0B = "#31748f",
+	base0C = "#9ccfd8",
+	base0D = "#c4a7e7",
+	base0E = "#f6c177",
+	base0F = "#C4A7E7",
 
-  base08 = "#d14d41", -- red
-  base09 = "#da702c", -- orange
-  base0A = "#d0a215", -- yellow
-  base0B = "#879a39", -- green
-  base0C = "#3aa99f", -- cyan
-  base0D = "#4385be", -- blue
-  base0E = "#8b7ec8", -- purple
-  base0F = "#ce5d97", -- magenta
+
 }
 
 return M
