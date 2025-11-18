de234f96288880b25a7840e8aaf12aea38d1852f
Esteban Gonzalez <esteban@estebandev.xyz>
2025-11-17 20:25:39 -0500

    update config

--- lua/palette.lua
+++ lua/palette.lua
@@ -1,22 +1,23 @@
 local M = {}
 
-M.palette = { -- My Color palette
-	base00 = "#191724",
-	base01 = "#1f1d2e",
-	base02 = "#26233a",
-	base03 = "#6e6a86",
-	base04 = "#908caa",
-	base05 = "#e0def4",
-	base06 = "#e0def4",
-	base07 = "#524f67",
-	base08 = "#eb6f92",
-	base09 = "#f6c177",
-	base0A = "#ebbcba",
-	base0B = "#31748f",
-	base0C = "#9ccfd8",
-	base0D = "#c4a7e7",
-	base0E = "#f6c177",
-	base0F = "#C4A7E7",
+M.palette = { -- Base16: jellybeans (custom)
+	base00 = "#151515", -- Default Background
+	base01 = "#1c1c1c", -- Lighter Background
+	base02 = "#333333", -- Selection / Alternate Background
+	base03 = "#606060", -- Comments / Inactive
+	base04 = "#888888", -- Dark Foreground
+	base05 = "#e8e8d3", -- Default Foreground
+	base06 = "#f0f0f0", -- Light Foreground
+	base07 = "#ffffff", -- White / Lightest
+
+	base08 = "#cf6a4c", -- Red (Error)
+	base09 = "#ffb964", -- Orange (Warning)
+	base0A = "#fad07a", -- Yellow (Attention)
+	base0B = "#70b950", -- Green (Success)
+	base0C = "#8fbfdc", -- Cyan (Info)
+	base0D = "#8197bf", -- Blue (Keyword)
+	base0E = "#c6b6ee", -- Purple (Special)
+	base0F = "#8fbfdc", -- Brown / Deprecated
 }
 
 return M
